f94ea58e1674b931aeaabd60a3fbbfd199e15019
Nandni1995 <nandnis95@gmail.com>
2016-07-05 13:41:23 +0530

    Create count2s.c

---
+++ count2s.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=1,b=0,n,i,j;
+clrscr();
+printf("Enter the value:");
+scanf("%d",&n);
+while(a!=n)
+{
+if(a==2)
+b++;
+else
+{
+i=n%10;
+j=n/10;
+if(i==2)
+b++;
+if(j==2)
+b++;
+}
+a++;
+}
+printf("%d",j);
+getch();
+}
